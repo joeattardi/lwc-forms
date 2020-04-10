4a51e4315dfb3442bceb29f27ddef05511d73c39
Joe Attardi <jattardi@salesforce.com>
2020-04-10 17:10:21 -0400

    Add touched, untouched, pristine, dirty classes

--- src/forms/formComponent.js
+++ src/forms/formComponent.js
@@ -1,55 +1,88 @@
 import { LightningElement } from 'lwc';
 
+const CLASS_DIRTY = 'lwc-dirty';
+const CLASS_PRISTINE = 'lwc-pristine';
+const CLASS_UNTOUCHED = 'lwc-untouched';
+const CLASS_TOUCHED = 'lwc-touched';
+
 export default class FormComponent extends LightningElement {
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this._lwcForm_handleInput = event => {
-      this.form.values[event.target.name] = event.target.value;
-    };
+        this._lwcForm_handleInput = (event) => {
+            this.form.values[event.target.name] = event.target.value;
+            event.target.classList.remove(CLASS_PRISTINE);
+            event.target.classList.add(CLASS_DIRTY);
+        };
 
-    this._lwcForm_handleSubmit = event => {
-      event.preventDefault();
-      this.formElement.dispatchEvent(new CustomEvent('formsubmit', { detail: this.form.values }));
-    };
-  }
+        this._lwcForm_handleSubmit = (event) => {
+            event.preventDefault();
+            this.formElement.dispatchEvent(
+                new CustomEvent('formsubmit', { detail: this.form.values })
+            );
+        };
 
-  initForm(formData) {
-    this.form = {
-      values: {}
-    };
+        this._lwcForm_handleFocus = (event) => {
+            if (event.target.classList.contains(CLASS_UNTOUCHED)) {
+                event.target.classList.remove(CLASS_UNTOUCHED);
+            }
+        };
 
-    formData.forEach(formField => {
-      this.form.values[formField.name] = formField.value;
-    });
-  }
-
-  renderedCallback() {
-    if (!this.rendered) {
-      this.formFields = {};
-
-      Object.keys(this.form.values).forEach(formFieldName => {
-        this.formFields[formFieldName] = this.template.querySelector(`[name=${formFieldName}]`);
-
-        if (!this.formElement) {
-          this.formElement = this.formFields[formFieldName].closest('form');
-          this.formElement.addEventListener('submit', this._lwcForm_handleSubmit);
-        }
-
-        this.formFields[formFieldName].addEventListener('input', this._lwcForm_handleInput);
-      });
-
-      this.rendered = true;
+        this._lwcForm_handleBlur = (event) => {
+            event.target.classList.remove(CLASS_UNTOUCHED);
+            event.target.classList.add(CLASS_TOUCHED);
+        };
     }
-  }
 
-  disconnectedCallback() {
-    console.log('removing listeners from form:', this.formElement);
-    this.formElement.removeEventListener('submit', this._lwcForm_handleSubmit);
+    initForm(formData) {
+        this.form = {
+            values: {}
+        };
 
-    Object.values(this.formFields).forEach(formField => {
-      console.log('removing listeners from:', formField);
-      formField.removeEventListener('input', this._lwcForm_handleInput);
-    });
-  }
+        formData.forEach((formField) => {
+            this.form.values[formField.name] = formField.value;
+        });
+    }
+
+    renderedCallback() {
+        if (!this.rendered) {
+            this.formFields = {};
+
+            Object.keys(this.form.values).forEach((formFieldName) => {
+                const formField = (this.formFields[
+                    formFieldName
+                ] = this.template.querySelector(`[name=${formFieldName}]`));
+
+                if (!this.formElement) {
+                    this.formElement = formField.closest('form');
+                    this.formElement.addEventListener(
+                        'submit',
+                        this._lwcForm_handleSubmit
+                    );
+                }
+
+                formField.addEventListener('input', this._lwcForm_handleInput);
+                formField.addEventListener('focus', this._lwcForm_handleFocus);
+                formField.addEventListener('blur', this._lwcForm_handleBlur);
+
+                formField.classList.add(CLASS_UNTOUCHED);
+                formField.classList.add(CLASS_PRISTINE);
+            });
+
+            this.rendered = true;
+        }
+    }
+
+    disconnectedCallback() {
+        this.formElement.removeEventListener(
+            'submit',
+            this._lwcForm_handleSubmit
+        );
+
+        Object.values(this.formFields).forEach((formField) => {
+            formField.removeEventListener('input', this._lwcForm_handleInput);
+            formField.removeEventListener('focus', this._lwcForm_handleFocus);
+            formField.removeEventListener('blur', this._lwcForm_handleBlur);
+        });
+    }
 }
